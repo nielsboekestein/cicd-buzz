b27fca094509b94271efd48def6c3bd5248ab883
nielsboekestein <niels.boekestein@student.hu.nl>
2020-12-18 14:38:00 +0100

    5.6

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -4,15 +4,15 @@ from buzz import generator
 def test_sample_single_word():
     een = ('foo', 'bar', 'foobar')
     word = generator.sample(l)
-    assert word in l
+    assert word in een
 
 
 def test_sample_multiple_words():
     een = ('foo', 'bar', 'foobar')
     words = generator.sample(l, 2)
     assert len(words) == 2
-    assert words[0] in l
-    assert words[1] in l
+    assert words[0] in een
+    assert words[1] in een
     assert words[0] is not words[1]
 
 def test_generate_buzz_of_at_least_five_words():
