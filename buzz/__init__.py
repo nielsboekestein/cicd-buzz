3712dab9a190e4d4f4e003619ee3c79b3cd19933
nielsboekestein <niels.boekestein@student.hu.nl>
2020-12-18 15:57:26 +0100

    def V1

--- buzz/__init__.py
+++ buzz/__init__.py
@@ -2,14 +2,16 @@ import os
 from flask import Flask
 from buzz import generator
 
-application = Flask(__name__)
+app = Flask(__name__)
 
-@application.route("/")
+
+@app.route("/")
 def generate_buzz():
     page = '<html><body><h1>'
     page += generator.generate_buzz()
     page += '</h1></body></html>'
     return page
 
+
 if __name__ == "__main__":
-    application.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
